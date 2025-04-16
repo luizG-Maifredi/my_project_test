3c621352d0250d6b9ba9e211da9bbe6a505d6128
lgmb <luizg.maifredi@gmail.com>
2025-04-15 22:18:24 -0300

    testes finais de api

--- GoogleMapsScreen.js
+++ GoogleMapsScreen.js
@@ -1,19 +1,25 @@
-import React, { useRef, useState } from "react";
-import { View, StyleSheet } from "react-native";
-import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import React, { useEffect, useRef, useState } from "react";
+import { View, StyleSheet, Alert, Text, Platform, PermissionsAndroid} from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Polygon} from "react-native-maps";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "./config/constants";
 import 'react-native-get-random-values';
 import MapViewDirections from "react-native-maps-directions";
+import * as geolib from 'geolib';
+import * as Location from 'expo-location';
 
 export default function GoogleMapsScreen() {
-  const mapRef = useRef(); 
+  const mapRef = useRef();
+  const [permissionGranter, setPermission] = useState(false); 
   const [origin, setOrigin] = useState();
   const [destination, setDestination] = useState();
+  const [marker, setMarker] = useState();
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
   const [markers, setMarkers] = useState([
     {
       id: 1,
-      latitude: -20.758210,
+      latitude: -20.758210,  
       longitude: -41.457238,
       title: "Pin 1 estou aqui",
       description: "Essa é a localização do pin 1",
@@ -27,6 +33,48 @@ export default function GoogleMapsScreen() {
     },
   ]);
 
+  useEffect(() => {
+    _getLocationPermission();
+  }, []);
+
+   async function _getLocationPermission() {
+        if(Platform.OS === "android"){
+          try {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              {
+                title: 'Permissão de Localização',
+                message:
+                  'Dar acesso a localização ?',
+                buttonNeutral: 'Ask Me Later',
+                buttonNegative: 'Cancel',
+                buttonPositive: 'OK',
+              },
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              console.log('PERMISSÃO A LOCALIZAÇÃO GARANTIDA');
+              setPermission(true);
+              const loc = await Location.getCurrentPositionAsync({});
+              console.log("LOCALIZAÇÃO:", loc);
+              setLocation(loc);
+              moveToLocation(loc.coords.latitude, loc.coords.longitude);
+              
+            } else {
+              console.log('PERMISSÃO A LOCALIZAÇÃO NEGADA');
+            }
+          } catch (err) {
+            console.warn(err);
+          }
+        }
+  }
+
+  let myPoligon = [
+    { "latitude": -20.84815, "longitude": -41.13063 },
+    { "latitude": -20.84750, "longitude": -41.12950 },
+    { "latitude": -20.84900, "longitude": -41.12850 },
+    { "latitude": -20.84950, "longitude": -41.12980 },
+    { "latitude": -20.84815, "longitude": -41.13063 }
+  ]
   async function moveToLocation(latitude, longitude) {
     if (mapRef.current) {
       mapRef.current.animateToRegion(
@@ -57,6 +105,27 @@ export default function GoogleMapsScreen() {
   ]);
 }
 
+function isInPolygon(coordinates){
+  let bol = geolib.isPointInPolygon(coordinates, myPoligon);
+  let msg = ""
+  console.log(bol)
+  if(bol){
+    msg = "o marcador está no ponto";
+  }
+  else{
+    msg = "o marcador não está no ponto";
+  }
+  Alert.alert('geofencing', msg);
+}
+
+
+if(!permissionGranter){
+  return(
+    <View>
+      <Text>Por favor de a permissão necessaria para continuar...</Text>
+    </View>    
+  )
+}
   return (
     <View style={styles.container}>
       {/* Barra de Pesquisa */}
@@ -72,6 +141,7 @@ export default function GoogleMapsScreen() {
               }  
               const { lat, lng } = details.geometry.location;
               setOrigin({latitude: originCordinates.latitude, longitude: originCordinates.longitude});
+
               console.log(origin)
               moveToLocation(originCordinates.latitude, originCordinates.longitude);
             }
@@ -150,15 +220,25 @@ export default function GoogleMapsScreen() {
       <MapView
         ref={mapRef}
         provider={PROVIDER_GOOGLE}
-        onPress={e => handleNewPin(e.nativeEvent.coordinate)}
+        //onPress={e => handleNewPin(e.nativeEvent.coordinate)}
+        onPress={e => {
+          console.log(e.nativeEvent.coordinate);
+          setMarker(e.nativeEvent.coordinate);
+          isInPolygon(e.nativeEvent.coordinate);
+        }}
         style={styles.map}
         initialRegion={{
-          latitude: -20.758114,
-          longitude: -41.461315,
+          latitude: -20.3155,
+          longitude: -40.3128,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
       >
+        {marker !== undefined ? 
+        <Marker coordinate={marker}
+        title="marker"
+        description="teste poligon"/> : null}
+
         {origin !== undefined ? 
         <Marker coordinate={origin}
         title="origem"
@@ -203,9 +283,20 @@ export default function GoogleMapsScreen() {
             apikey={GOOGLE_MAPS_API_KEY}
             strokeColor="blue"
             strokeWidth={3}
+            onReady={result => {
+              console.log(`Distância: ${result.distance} km`); // Distância em KM
+              console.log(`Duração: ${result.duration} min`); // Tempo estimado
+            }}
             mode="DRIVING"
+            
           />
         : null}
+
+        <Polygon strokeColor="red"
+        fillColor="#EBF5F8"
+        strokeWidth={2}
+        coordinates={myPoligon}/>
+
       </MapView>
     </View>
   );
